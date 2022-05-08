3fef35525c64965aad6b52a945ac2ba4101a229e
ali Shaker <alishakeri@hotmail.fr>
2022-05-08 13:25:29 +0200

    ajout des cpt terminé

--- public/content/plugins/oprofile/class/plugin.php
+++ public/content/plugins/oprofile/class/plugin.php
@@ -6,24 +6,38 @@ class Plugin
 {
     public function __construct()
     {
-        // grace a la ligne suivante je viens me racorder a la classe registration ! 
-        // ainsi, tout ce que j'aurais codé en plus dans le constructeur de ma classe Plugin
-        // va se retrouver dans le constructeur de la classe Registration !
-        // $registration = new Registration();
-
         // Dans ce constructeur je vais venir fabriquer mes premiers CPT :D !! 
         // au moment de l'initialisation de Wordpress
         add_action(
             'init',
             [$this, 'createProjectCustomPostType']
         );
+
+        add_action(
+            'init',
+            [$this, 'createCustomerProfileCustomPostType']
+        );
+
+        add_action(
+            'init',
+            [$this, 'createDeveloperProfileCustomPostType']
+        );
+
         add_action(
             'init',
             [$this, 'createTechnologyCustomTaxonomie']
         );
+
+        add_action(
+            'init',
+            [$this, 'createActivitySectorTaxonomie']
+        );
+
+        add_action(
+            'init',
+            [$this, 'createSkillCustomTaxonomie']
+        );
     }
-
-
     /**
      * createProjectCustomPostType
      * Fabrication de notre tout premier CPT (Custom Post Type) !! 
@@ -45,8 +59,55 @@ class Plugin
                     'thumbnail',
                     'editor'
                 ],
-                // 'capability_type' => 'project',
-                // 'map_meta_cap' => true
+                'capability_type' => 'project',
+                'map_meta_cap' => true
+            ]
+        );
+    }
+
+    public function createDeveloperProfileCustomPostType()
+    {
+        register_post_type(
+            // identifiant du post type
+            'developer-profile',
+            // les options pour configurer le post type
+            [   // intitulé
+                'label' => 'Developer Profile',
+                'public' => true,
+                'hierarchical' => false,
+                'menu_icon' => 'dashicons-admin-users',
+                // fonctionnalités activable poure un cpt :  ‘title’, ‘editor’, ‘comments’, ‘revisions’, ‘trackbacks’, ‘author’, ‘excerpt’, ‘page-attributes’, ‘thumbnail’, ‘custom-fields’, and ‘post-formats’.
+                'supports' => [
+                    'author',
+                    'title',
+                    'thumbnail',
+                    'editor'
+                ],
+                'capability_type' => 'developer',
+                'map_meta_cap' => true
+            ]
+        );
+    }
+
+    public function createCustomerProfileCustomPostType()
+    {
+        register_post_type(
+            // identifiant du post type
+            'customer-profile',
+            // les options pour configurer le post type
+            [   // intitulé
+                'label' => 'Customer Profile',
+                'public' => true,
+                'hierarchical' => false,
+                'menu_icon' => 'dashicons-admin-users',
+                // fonctionnalités activable poure un cpt :  ‘title’, ‘editor’, ‘comments’, ‘revisions’, ‘trackbacks’, ‘author’, ‘excerpt’, ‘page-attributes’, ‘thumbnail’, ‘custom-fields’, and ‘post-formats’.
+                'supports' => [
+                    'title',
+                    'thumbnail',
+                    'editor'
+                ],
+                'capability_type' => 'customer', //! pour customizer les capability par defaut wp
+                'map_meta_cap' => true //!ajouter plus de capability 
             ]
         );
     }
@@ -61,7 +122,7 @@ class Plugin
             //identifiant taxonomie 
             'technology',
             // cette "étiquette" pourra etre utilisé sur le CPT project
-            'project',
+            ['project', 'developer-profile'],
             // tableau d'options
             [
                 'label' => 'Technologie',
@@ -71,6 +132,37 @@ class Plugin
         );
     }
 
+    public function createSkillCustomTaxonomie()
+    {
+        register_taxonomy(
+            //identifiant taxonomie 
+            'skill',
+            // cette "étiquette" pourra etre utilisé sur le CPT project
+            ['developer-profile'],
+            // tableau d'options
+            [
+                'label' => 'Compétence',
+                'hierarchical' => false,
+                'public' => true
+            ]
+        );
+    }
+
+    public function createActivitySectorTaxonomie()
+    {
+        register_taxonomy(
+            //identifiant taxonomie 
+            'activity-sector',
+            // cette "étiquette" pourra etre utilisé sur le CPT project
+            ['customer-profile'],
+            // tableau d'options
+            [
+                'label' => 'Secteur d\'activité',
+                'hierarchical' => false,
+                'public' => true
+            ]
+        );
+    }
     /**
      * activate
      * Methode executée lors de l'activation du plugin
